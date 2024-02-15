304c19d0d2ff7f196b7a01f21c2d1cca7d08f350
Kreyparion <kreyparion@gmail.com>
2024-02-15 17:49:40 +0100

    add optional verbose

--- run.sh
+++ run.sh
@@ -1,11 +1,17 @@
-# check if the number of arguments is correct
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <tab_number[1:17]> <verbose{1,2,3}>"
+# check if the number of arguments is one or two
+if [ -z "$1" ]; then
+    echo "Usage: $0 <tab_number[1:17]> (verbose{1,2,3})"
     exit 1
+fi
+
+# default value for $2 is 0
+default=0
+if [ -n "$2" ]; then
+    default=$2
 fi
 
 cd build
 cmake ../src/
 make
-./vrp $1 $2
+./vrp $1 $default
 
