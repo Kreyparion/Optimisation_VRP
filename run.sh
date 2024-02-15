fee0b4684b1db50dc7702c4ec75b008f87510d6f
Kreyparion <kreyparion@gmail.com>
2024-02-15 17:16:32 +0100

    add verbose

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
+# check if the number of arguments is correct
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <tab_number[1:17]> <verbose{1,2,3}>"
+    exit 1
+fi
+
 cd build
 cmake ../src/
 make
-./vrp $1
+./vrp $1 $2
+
